d1aefbf71b2272124c8f3d1b743161076f69d353
Deepika602 <deepika.polasani@gmail.com>
2020-02-25 11:17:05 -0800

    naveen project commit

---
+++ src/test/java/com/crm/qa/testcases/DemoTest.java
@@ -0,0 +1,12 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+  @Test
+  public void f() {
+	  
+	  Assert.assertTrue(true);
+  }
+}
